0094a100e1a0a93d6028dae7ce1ca877fe313b8b
venus0720 <venusdev@eclipso.email>
2021-10-21 22:01:50 +0800

    restructured files'

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 import React from 'react'
 
-import Navbar from 'components/Navbar/Navbar'
-import LandingPageHero from 'sections/Hero/LandingPageHero'
-import LandingPageFeatures from 'sections/LandingPageFeatures/LandingPageFeatures'
-import LandingPageGraph from 'sections/LandingPageGraph/LandingPageGraph'
-import LandingPageCTA from 'components/CTA/LandingPageCTA'
-import Footer from 'components/Footer/Footer'
+import LandingPage from 'pages/LandingPage'
 
 const App = () => {
     return (
         <div>
-            <div>
-                <Navbar />
-            </div>
-            <div>
-                <LandingPageHero />
-            </div>
-            <div className="pt-20">
-                <LandingPageFeatures />
-            </div>
-            <div className="">
-                <LandingPageGraph />
-            </div>
-            <div>
-                <LandingPageCTA />
-            </div>
-            <div>
-                <Footer />
-            </div>
+            <LandingPage />
         </div>
     )
 }
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Stacked from 'layout/Stacked'
+
+import LandingPageHero from 'sections/Hero/LandingPageHero'
+import LandingPageFeatures from 'sections/LandingPageFeatures/LandingPageFeatures'
+import LandingPageGraph from 'sections/LandingPageGraph/LandingPageGraph'
+import LandingPageCTA from 'components/CTA/LandingPageCTA'
+
+
+const LandingPage = () => {
+    return (
+        <>
+            <Stacked>
+                <LandingPageHero />
+                <LandingPageFeatures />
+                <LandingPageGraph />
+                <LandingPageCTA />
+            </Stacked>
+        </>
+    )
+
+}
+
+export default LandingPage
