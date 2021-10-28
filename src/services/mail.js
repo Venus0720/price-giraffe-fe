ac494e4839257ccbe79abafa871d8468f56e64e8
venus0720 <venusdev@eclipso.email>
2021-10-29 00:01:02 +0800

    console logs

--- src/services/mail.js
+++ src/services/mail.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 
 const registerMailList = async (email) => {
     const response = await axios.post('/api/v1/mail_list', email)
+    console.log(response)
+    console.log(response.data)
     return response.data
 }
 
